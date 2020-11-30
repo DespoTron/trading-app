7a225262f41389b3b59222d6d652d92435e4fa6c
Thai Do <thai.hl.do@gmail.com>
2020-11-30 15:28:59 -0800

    Created mock layout to confirm what should go into Header

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import { Header } from './component'
 
 function App() {
   return (
     <div className="App">
-      {*/ Header */}
+      {/* Header */}
       <div className="app__header">
         <Header />
       </div>
       <div className="app__body">
-        
+
       </div>
     </div>
   );
---
+++ src/component/Header/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default () => {
+  return (
+    <div className="header__wrapper">
+      {/* logo */}
+      <div className="header__logo">
+        TEST
+      </div>
+      {/* search */}
+      {/* menu items */}
+    </div>  
+  )
+}
