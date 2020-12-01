e67f88dac9e290621ffb52ffee9c3196c9d345ab
Thai Do <thai.hl.do@gmail.com>
2020-11-30 20:27:43 -0800

    Working with React Chartjs

--- src/component/LineGraph/index.js
+++ src/component/LineGraph/index.js
@@ -9,6 +9,9 @@ export default () => {
   }, {
     x: 15,
     y: 10
+  }, {
+    x: 12,
+    y: 4
   }]
 
   return (
@@ -18,9 +21,30 @@ export default () => {
           datasets: [
             {
               type: "line",
-              data: data
+              data: data,
+              backgroundColor: "black",
+              borderColor: "#5AC53B",
+              borderWidth: 2,
+              pointBorderColor: 'rgba(0, 0, 0, 0)',
+              pointHoverBackgroundColor: '#5AC53B',
+              pointHoverBorderColor: '#000000',
+              pointHoverBorderWidth: 4,
+              pointHoverRadius: 6,
             }
           ]
+        }}
+        options={{
+          legend: {
+            display: false
+          },
+          scales: {
+            yAxes: [{
+              ticks: {
+                display: false,
+                beginAtZero: true
+              }
+            }]
+          }
         }}
       />
     </div>
