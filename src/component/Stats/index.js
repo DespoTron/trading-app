64d850cde1a54928c49b7e8a76e9099f67f55cee
Thai Do <thai.hl.do@gmail.com>
2020-12-01 12:14:18 -0800

    Added styling to two components

--- src/component/Stats/index.js
+++ src/component/Stats/index.js
@@ -1,10 +1,26 @@
 import React from 'react'
+import './styles.css'
 
 export default () => {
   return (
     <div className="stats">
       <div className="stats__container">
-        
+        <div className="stats__header">
+          <p>Stocks</p>
+        </div>
+        <div className="stats__content">
+          <div className="stats__rows">
+{/* for our current stocks */}
+          </div>
+        </div>
+        <div className="stats__header">
+          <p>Lists</p>
+        </div>
+        <div className="stats__content">
+          <div className="stats__rows">
+{/* stocks we can buy */}
+          </div>
+        </div>
       </div>
     </div>
   )
