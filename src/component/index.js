8648dc21d3c02e72985d1ee7f5c1aa4d3b7d0f6d
Thai Do <thai.hl.do@gmail.com>
2020-11-30 20:08:56 -0800

    Added styling for two components and code

--- src/component/index.js
+++ src/component/index.js
@@ -1,3 +1,4 @@
 export {default as Header} from './Header'
 export {default as Newsfeed} from './Newsfeed'
 export {default as Stats} from './Stats'
+export {default as LineGraph} from './LineGraph'
