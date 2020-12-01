617a9557ab665d6c3c12411c0ba5d5e79fd88f34
Thai Do <thai.hl.do@gmail.com>
2020-12-01 13:51:39 -0800

    Added hardcoded data to test if everything works

--- src/component/Stats/index.js
+++ src/component/Stats/index.js
@@ -1,43 +1,51 @@
-import React, { useState, useEffect } from 'react'
-import axios from 'axios'
-import './styles.css'
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { StatsRow } from 'component'
+import "./styles.css";
 
 const BASE_URL = process.env.REACT_APP_BASE_URL;
 const authToken = process.env.REACT_APP_FINN_HUB_TOKEN;
 
 export default () => {
-
-  const [stockData, setStockData] = useState([])
+  const [stockData, setStockData] = useState([]);
 
   const getStockData = (stock) => {
     return axios
       .get(`${BASE_URL}?symbol=${stock}&token=${authToken}`)
       .catch((error) => {
         console.error("Error", error.message);
-      })
-  }
+      });
+  };
 
   useEffect(() => {
     let tempStockData = [];
-    const stockList = ["AAPL", "MSFT", "TSLA", "FB", "BABA", "UBER", "DIS", "SBUX"];
+    const stockList = [
+      "AAPL",
+      "MSFT",
+      "TSLA",
+      "FB",
+      "BABA",
+      "UBER",
+      "DIS",
+      "SBUX",
+    ];
     let promises = [];
     stockList.map((stock) => {
       promises.push(
-        getStockData(stock)
-        .then((res) => {
+        getStockData(stock).then((res) => {
           tempStockData.push({
             name: stock,
-            ...res.data
+            ...res.data,
           });
         })
-      )
+      );
     });
 
-    Promise.all(promises).then(() =>{
-      setStockData(tempStockData)
+    Promise.all(promises).then(() => {
+      setStockData(tempStockData);
       console.log(tempStockData);
-    })
-  }, [])
+    });
+  }, []);
 
   return (
     <div className="stats">
@@ -46,19 +54,24 @@ export default () => {
           <p>Stocks</p>
         </div>
         <div className="stats__content">
-          <div className="stats__rows">
-{/* for our current stocks */}
-          </div>
+          <div className="stats__rows"></div>
         </div>
         <div className="stats__header">
           <p>Lists</p>
         </div>
         <div className="stats__content">
           <div className="stats__rows">
-{/* stocks we can buy */}
+            {stockData.map((stock) => (
+              <StatsRow
+                // key={stock.name}
+                // name={stock.name}
+                // openPrice={stock.o}
+                // price={stock.c}
+              />
+            ))}
           </div>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
---
+++ src/component/StatsRow/index.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+export default () => {
+  return (
+    <div className="row">
+      <div className="row__intro">
+        <h1>AAPL</h1>
+        <p>200 shares</p>
+      </div>
+      <div className="row__chart">
+        {/* <img src={StockChart} height={16} /> */}
+      </div>
+      <div className="row__numbers">
+        <p className="row__price">$200</p>
+        <p className="row__percentage">+200%</p>
+      </div>
+
+    </div>
+  )
+}
