011c4770b834554ff1d37e1384d959adb50007b2
Thai Do <thai.hl.do@gmail.com>
2020-12-01 14:53:15 -0800

    Got firebase hooked up and added dependencies

--- src/component/Stats/index.js
+++ src/component/Stats/index.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { StatsRow } from 'component'
+import { db } from '../../firebase'
 import "./styles.css";
 
 const BASE_URL = process.env.REACT_APP_BASE_URL;
@@ -8,6 +9,30 @@ const authToken = process.env.REACT_APP_FINN_HUB_TOKEN;
 
 export default () => {
   const [stockData, setStockData] = useState([]);
+  const [myStocks, setMyStocks] = useState([]);
+
+  const getMyStocks = () => {
+    db
+    .collection('myStocks')
+    .onSnapshot(snapshot => {
+      let promises = [];
+      let tempData = []
+      snapshot.docs.map((doc) => {
+
+        promises.push(getStockData(doc.data().ticker)
+        .then(res => {
+          tempData.push({
+            id: doc.id,
+            data: doc.data(),
+            info: res.data
+          })
+        })
+      )})
+      Promise.all(promises).then(() => {
+        setMyStocks(tempData);
+      })
+    })
+  }
 
   const getStockData = (stock) => {
     return axios
@@ -30,6 +55,7 @@ export default () => {
       "SBUX",
     ];
     let promises = [];
+    getMyStocks();
     stockList.map((stock) => {
       promises.push(
         getStockData(stock).then((res) => {
@@ -43,7 +69,6 @@ export default () => {
 
     Promise.all(promises).then(() => {
       setStockData(tempStockData);
-      console.log(tempStockData);
     });
   }, []);
 
@@ -61,12 +86,13 @@ export default () => {
         </div>
         <div className="stats__content">
           <div className="stats__rows">
-            {stockData.map((stock) => (
+            {myStocks.map((stock) => (
               <StatsRow
-                key={stock.name}
-                name={stock.name}
-                openPrice={stock.o}
-                price={stock.c}
+                key={stock.data.ticker}
+                name={stock.data.ticker}
+                openPrice={stock.info.o}
+                shares={stock.data.shares}
+                price={stock.info.c}
               />
             ))}
           </div>
